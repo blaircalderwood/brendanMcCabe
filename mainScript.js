6bb1df245f408da44085a8b124df1997cec53dcf
blaircalderwood@live.co.uk <blaircalderwood@live.co.uk>
2016-01-09 16:47:59 +0000

    Smooth scrolling when anchor clicked implemented

--- mainScript.js
+++ mainScript.js
@@ -5,14 +5,32 @@ window.onload = function(){
 
 };
 
+//Change navbar height on window resize
 function resizeWindow(){
 
     $("#mainImage").height($(window).height() - $("#mainNavbar").height());
 
 }
 
+//Affix navbar when scrolling past
 $('#nav').affix({
     offset: {
         top: $('#mainNav').offset().top
     }
+});
+
+//Smooth Scrolling
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
